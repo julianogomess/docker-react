ea6bb8c299c00ae67b67bc889205f8a1c37561d4
Juliano Gomes Sousa <julianosousa@ibm.com>
2021-08-19 09:34:31 -0300

    add text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prazer<code>src/App.js</code> .
+          Ola<code>src/App.js</code> .
         </p>
         <a
           className="App-link"
